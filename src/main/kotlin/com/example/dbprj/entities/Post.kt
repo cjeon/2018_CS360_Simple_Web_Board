eaab02cac0e98672b40c08b70002d17096c5883c
Cheolho Jeon <adobecs5.kaist@gmail.com>
2018-11-06 09:15:02 +0900

    code clean up, gitignore update
    Post - remove public keyword, URL - delete file (unused), gitignore - I don't know why it was not uploaded in the first place

--- src/main/kotlin/com/example/dbprj/entities/Post.kt
+++ src/main/kotlin/com/example/dbprj/entities/Post.kt
@@ -9,21 +9,21 @@ import javax.persistence.*
 // table definition
 @Entity
 @Table(name = "Post")
-public data class Post(@Id
-                       @GeneratedValue(strategy = GenerationType.AUTO)
-                       var id: Long? = null,
-                       @field:CreationTimestamp
-                       @Temporal(TemporalType.TIMESTAMP)
-                       @Column(name = "created_at", nullable = false)
-                       var createdAt: Date? = null,
-                       @NotNull
-                       @Temporal(TemporalType.TIMESTAMP)
-                       @field:UpdateTimestamp
-                       @Column(name = "updated_at", nullable = false)
-                       var updatedAt: Date? = null,
-                       @NotNull
-                       @Column(nullable = false)
-                       var title: String? = null,
-                       @NotNull
-                       @Column(nullable = false)
-                       var text: String? = null)
+data class Post(@Id
+                @GeneratedValue(strategy = GenerationType.AUTO)
+                var id: Long? = null,
+                @field:CreationTimestamp
+                @Temporal(TemporalType.TIMESTAMP)
+                @Column(name = "created_at", nullable = false)
+                var createdAt: Date? = null,
+                @NotNull
+                @Temporal(TemporalType.TIMESTAMP)
+                @field:UpdateTimestamp
+                @Column(name = "updated_at", nullable = false)
+                var updatedAt: Date? = null,
+                @NotNull
+                @Column(nullable = false)
+                var title: String? = null,
+                @NotNull
+                @Column(nullable = false)
+                var text: String? = null)
--- src/main/kotlin/com/example/dbprj/entities/Url.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dbprj.entities
-
-import javax.persistence.*
-
-@Entity
-@Table(name = "Url")
-data class Url(@Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Int,
-               var url: String)
