53017c36dd920453368bab16aaac2e413a77fd6a
Cheolho Jeon <adobecs5.kaist@gmail.com>
2018-11-06 11:07:56 +0900

    Add data seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user` (`id`, `created_at`, `is_admin`, `password`, `updated_at`, `user_id`) VALUES (1,'2018-11-06 00:00:00',b'1','admin','2018-11-06 00:00:00','admin');
+INSERT INTO `user` (`id`, `created_at`, `is_admin`, `password`, `updated_at`, `user_id`) VALUES (2,'2018-11-06 00:00:00',b'0','user','2018-11-06 00:00:00','user');
