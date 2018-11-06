93e1203f2f151f1daa2a92b776b7e3c5c202d291
Cheolho Jeon <adobecs5.kaist@gmail.com>
2018-11-06 11:09:03 +0900

    Add user validation process
    id, password 체크하는 로직 추가. 변수명 변경 & 필드 추가에 따른 html 수정.
    
    Post를 보여줄 때 author도 보여줘야하고 Post db에 UID도 링크걸어줘야함. (FK)

--- src/main/kotlin/com/example/dbprj/controllers/PostController.kt
+++ src/main/kotlin/com/example/dbprj/controllers/PostController.kt
@@ -1,6 +1,7 @@
 package com.example.dbprj.controllers
 
 import com.example.dbprj.DbImpls.PostServiceImpl
+import com.example.dbprj.DbImpls.UserServiceImpl
 import com.example.dbprj.entities.Post
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.lang.NonNull
@@ -14,20 +15,48 @@ import org.springframework.web.bind.annotation.PostMapping
 @Controller
 class PostController {
     @Autowired
-    @NonNull var postServiceImpl: PostServiceImpl? = null
+    @NonNull
+    var postServiceImpl: PostServiceImpl? = null
+
+    @Autowired
+    @NonNull
+    var userServiceImpl: UserServiceImpl? = null
 
     @GetMapping("/post")
     fun getPost(model: Model): String {
-        model.addAttribute("post", Post())
+        model.addAttribute("post_payload", PostPayload())
         return "post"
     }
 
     @PostMapping("/post")
-    fun postPost(@ModelAttribute post: Post): String {
-        if (post.title == null || post.text == null) {
-            return ""
+    fun postPost(@ModelAttribute postPayload: PostPayload): String {
+        if (!postPayload.validatePayload(userServiceImpl)) {
+            return "error"
         }
-        postServiceImpl?.createPost(post)
+        postServiceImpl?.createPost(postPayload.toPostEntity())
         return "view"
     }
+}
+
+data class PostPayload(var userId: String? = null,
+                       var password: String? = null,
+                       var title: String? = null,
+                       var text: String? = null) {
+    /**
+     * validates the payload.
+     * 1. Payload should have valid id & password.
+     * 2. Payload should have non-null title & text.
+     *
+     * @return true if payload is valid, false if payload is invalid.
+     */
+    fun validatePayload(userServiceImpl: UserServiceImpl?): Boolean {
+        if (userServiceImpl == null) return false
+        val finalUserId = userId
+        val finalPassword = password
+        if (finalUserId == null || finalPassword == null) return false
+        val results = userServiceImpl.findByUserId(finalUserId)
+        return results.firstOrNull { it.password == finalPassword } != null
+    }
+
+    fun toPostEntity() = Post(title=this.title, text=this.text)
 }
