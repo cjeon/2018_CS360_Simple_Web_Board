07d81ed36fe986fc21f3bbae932dd79d5fa99170
Cheolho Jeon <adobecs5.kaist@gmail.com>
2018-11-13 11:03:36 +0900

    Validate User logic 추가
    여러곳에서 사용되는 경우가 많아서 PostServiceImpl에 추가함.

--- src/main/kotlin/com/example/dbprj/DbImpls/PostServiceImpl.kt
+++ src/main/kotlin/com/example/dbprj/DbImpls/PostServiceImpl.kt
@@ -14,4 +14,23 @@ class PostServiceImpl @Autowired constructor(val repo: PostRepository) : PostSer
         return post
     }
 
+    /**
+     * validate 되었으면 Post 를 return. else, null.
+     */
+    fun validateUser(postId: String, userId: String?, userPassword: String?): Post? {
+        val postOptional = repo.findById(postId.toLong())
+        if (!postOptional.isPresent) return null
+        val post = postOptional.get()
+        val user = post.user ?: return null
+        // 글의 작성자이면
+        if (user.id.toString() == userId && user.password == userPassword) {
+            return post
+        }
+        // 유저가 어드민이면
+        if (user.isAdmin == true) {
+            return post
+        }
+        // 해당이 없으면
+        return null
+    }
 }
