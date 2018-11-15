b87a12093f887322835e8386d1396d05e448a76b
Cheolho Jeon <adobecs5.kaist@gmail.com>
2018-11-15 09:22:22 +0900

    remove unused class

--- src/main/kotlin/com/example/dbprj/DALs/PostDAL.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dbprj.DALs
-
-import com.example.dbprj.entities.Post
-
-interface PostDAL {
-    fun fuzzySearchPost(query: String): List<Post>
-    fun wildCardSearchPost(query: String): List<Post>
-}
