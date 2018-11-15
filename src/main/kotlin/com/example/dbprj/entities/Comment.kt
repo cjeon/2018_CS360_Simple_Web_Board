e0b40f9dcb4aefb2dc08169b1d68dbe21fd6ffb2
Cheolho Jeon <adobecs5.kaist@gmail.com>
2018-11-15 10:40:47 +0900

    add comment table

---
+++ src/main/kotlin/com/example/dbprj/entities/Comment.kt
@@ -0,0 +1,40 @@
+package com.example.dbprj.entities
+
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import org.jetbrains.annotations.NotNull
+import java.util.*
+import javax.persistence.*
+
+// table definition
+@Entity
+@Table(name = "Comment")
+data class Comment(@Id
+                @GeneratedValue(strategy = GenerationType.IDENTITY)
+                var id: Long? = null,
+                @field:CreationTimestamp
+                @Temporal(TemporalType.TIMESTAMP)
+                @Column(name = "created_at", nullable = false)
+                var createdAt: Date? = null,
+                @NotNull
+                @Temporal(TemporalType.TIMESTAMP)
+                @field:UpdateTimestamp
+                @Column(name = "updated_at", nullable = false)
+                var updatedAt: Date? = null,
+                /*
+                * todo ADD text, user, post column
+                * see Post.kt for help
+                * column names should be `text`, `post_id`, `user_id`
+                * */
+                @NotNull
+                @Column(nullable = false)
+                var text: String? = null,
+                @NotNull
+                @ManyToOne
+                @JoinColumn(name = "user_id", nullable = false)
+                var user: User? = null,
+                @NotNull
+                @ManyToOne
+                @JoinColumn(name = "post_id", nullable = false)
+                var post: Post? = null
+)
