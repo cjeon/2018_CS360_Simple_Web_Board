1d9fab126de61b446930e1d1c3cc378215e7d87c
bsb0579 <40688086+bsb0579@users.noreply.github.com>
2018-11-20 07:50:05 +0900

    Add Report entity
    report entity :  reportCount, post, user

---
+++ src/main/kotlin/com/example/dbprj/entities/Report.kt
@@ -0,0 +1,35 @@
+package com.example.dbprj.entities
+
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import org.jetbrains.annotations.NotNull
+import java.util.*
+import javax.persistence.*
+
+// table definition
+@Entity
+@Table(name = "Report")
+data class Report(@Id
+                  @GeneratedValue(strategy = GenerationType.AUTO)
+                  var id: Long? = null,
+                  @field:CreationTimestamp
+                  @Temporal(TemporalType.TIMESTAMP)
+                  @Column(name = "created_at", nullable = false)
+                  var createdAt: Date? = null,
+                  @NotNull
+                  @Temporal(TemporalType.TIMESTAMP)
+                  @field:UpdateTimestamp
+                  @Column(name = "updated_at", nullable = false)
+                  var updatedAt: Date? = null,
+                  @NotNull
+                  @Column(nullable = false)
+                  var reportCount: Int? = 0,
+                  @NotNull
+                  @ManyToOne
+                  @JoinColumn(name = "post_id", nullable = false)
+                  var post: Post? = null,
+                  @NotNull
+                  @ManyToOne
+                  @JoinColumn(name = "user_id", nullable = false)
+                  var user: User? = null
+)
