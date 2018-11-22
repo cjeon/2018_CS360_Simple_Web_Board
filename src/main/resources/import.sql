fc820555c74ff1346d6428953bbd99443f413ad8
Cheolho Jeon <adobecs5.kaist@gmail.com>
2018-11-22 11:22:12 +0900

    Add create view statement

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `user` (`id`, `created_at`, `is_admin`, `password`, `updated_at`, `user_id`) VALUES (1,'2018-11-06 00:00:00',b'0','admin','2018-11-06 00:00:00','admin');
-INSERT INTO `user` (`id`, `created_at`, `is_admin`, `password`, `updated_at`, `user_id`) VALUES (2,'2018-11-06 00:00:00',b'1','user','2018-11-06 00:00:00','user');
+INSERT INTO `user` (`id`, `created_at`, `is_admin`, `password`, `updated_at`, `user_id`) VALUES (1,'2018-11-06 00:00:00',b'1','admin','2018-11-06 00:00:00','admin');
+INSERT INTO `user` (`id`, `created_at`, `is_admin`, `password`, `updated_at`, `user_id`) VALUES (2,'2018-11-06 00:00:00',b'0','user','2018-11-06 00:00:00','user');
 INSERT INTO `post` (`id`, `created_at`, `text`, `title`, `updated_at`, `user_id`) VALUES (1, '2018-11-12 14:45:07', '1', '1', '2018-11-12 14:45:07', '1');
 INSERT INTO `comment` (`id`, `created_at`, `text`, `updated_at`, `post_id`, `user_id`) VALUES (1, '2018-11-15 00:00:00', 'comment 1', '2018-11-15 00:00:00', 1, 1), (2, '2018-11-15 00:00:00', 'comment 2', '2018-11-15 00:00:00', 1, 2);
-
+create view `user_id_pw` as select u.id, u.user_id, u.password, u.is_admin from user as u;
